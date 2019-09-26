a5b9ba51ee4fef3fa1b469e0e7aa02ca53b4eca2
Onlygky <42513597+OnlyGky@users.noreply.github.com>
2019-09-26 19:40:34 +0800

    1025. 除数博弈

---
+++ db/sep_2019/sep_26_1025.java
@@ -0,0 +1,10 @@
+package db.sep_2019;
+//1025. 除数博弈
+public class sep_26_1025 {
+
+    public boolean divisorGame(int N) {
+        if (N%2 == 0)
+            return true;
+        return false;
+    }
+}
