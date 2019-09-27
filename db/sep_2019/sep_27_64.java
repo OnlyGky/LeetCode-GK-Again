61f7765d274df4a398d10fb26ad9c7e359cc53f7
Onlygky <42513597+OnlyGky@users.noreply.github.com>
2019-09-27 18:46:35 +0800

    64 最小路径和

---
+++ db/sep_2019/sep_27_64.java
@@ -0,0 +1,14 @@
+package db.sep_2019;
+
+public class sep_27_64 {
+    public int minPathSum(int[][] grid) {
+        for(int i=1;i<grid.length;i++)  grid[i][0]+=grid[i-1][0];
+        for(int j=1;j<grid[0].length;j++) grid[0][j]+=grid[0][j-1];
+        for(int i=1;i<grid.length;i++) {
+            for(int j=1;j<grid[0].length;j++) {
+                grid[i][j] = Math.min(grid[i][j-1], grid[i-1][j]) + grid[i][j];
+            }
+        }
+        return grid[grid.length-1][grid[0].length-1];
+    }
+}
