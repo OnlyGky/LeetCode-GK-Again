b3956e005bc59a9a2ed22777ffba0a3edf1d8c42
Onlygky <42513597+OnlyGky@users.noreply.github.com>
2019-12-01 16:20:28 +0800

    110. 平衡二叉树

---
+++ tree/nov/nov_29_110.java
@@ -0,0 +1,16 @@
+package tree.nov;
+
+public class nov_29_110 {
+    public boolean isBalanced(TreeNode root) {
+        return depth(root) != -1;
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) return 0;
+        int left = depth(root.left);
+        if(left == -1) return -1;
+        int right = depth(root.right);
+        if(right == -1) return -1;
+        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+    }
+}
